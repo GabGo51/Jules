1c79c5c72ff8a3a81e6df32a82fd58f6592cd536
jjjulesnadeau <jjjules.n@gmail.com>
2026-02-21 21:09:41 -0500

    cleanedup

--- src/App.jsx
+++ src/App.jsx
@@ -59,59 +59,49 @@ function App() {
             <Video gridColumn="2 /11 " name="nuitblue_03" margin="8%"  />
           </Layer>
           <Layer>
-            <Video gridColumn="4 /13 " name="mapp_02" margin="8%" />
-            <Video gridColumn="1 /4" name="mapp_03" margin="-60%" />
+            <Video gridColumn="5 /13 " name="mapp_02" margin="8%" />
+            <Image gridColumn="1 /4" name="wales_01" margin="20%" />
           </Layer>
           <Layer>
-            <Image gridColumn="11 /12 " name="wales_01" margin="80%" />
+            <Video gridColumn="9 /12 " name="mapp_03" margin="-50%" />
           </Layer>
           <Layer>
-            <Image gridColumn="2 /6 " name="anatolie_03" margin="-20%" />
-            <Image gridColumn="7 /13 " name="anatolie_02" margin="60%" />
+            <Image gridColumn="2 /8 " name="anatolie_03" margin="-20%" />
+            <Image gridColumn="10 /13 " name="anatolie_02" margin="180%" />
           </Layer>
           <Layer>
-            <Image gridColumn="2 /6 " name="anatolie_01" margin="-30%" />
-            <Image gridColumn="4 /12 " name="anatolie_04" margin="10%" />
+            <Image gridColumn="6 /12 " name="anatolie_04" margin="10%" />
           </Layer>
           <Layer>
-            <Image gridColumn="1 /8 " name="triplex_01" margin="80%" />
-            <Image gridColumn="10 /13 " name="triplex_03" margin="20%" />
+            <Image gridColumn="2 /7 " name="triplex_01" margin="60%" />
+            <Image gridColumn="11 /12 " name="triplex_03" margin="220%" />
           </Layer>
           <Layer>
-            <Image gridColumn="8 /12 " name="triplex_04" margin="-80%" />
+            <Image gridColumn="9 /11 " name="triplex_04" margin="-80%" />
           </Layer>
           <Layer>
-            <Video gridColumn="1 /7 " name="Ada" margin="" />
+            <Video gridColumn="1 /7 " name="Ada" margin="-20%" />
           </Layer>
           <Layer>
-            <Video gridColumn="2 /12 " name="foil_01" margin="" />
+            <Video gridColumn="9 /13 " name="AllianceNav" margin="" />
           </Layer>
           <Layer>
-            <Video gridColumn="3 /12 " name="AllianceNav" margin="" />
+            <Image gridColumn="2 /4 " name="farley_01" margin="50%" />
+            <Image gridColumn="8 /11 " name="wales_02" margin="140%" />
           </Layer>
           <Layer>
-            <Image gridColumn="2 /5 " name="farley_01" margin="20%" />
-            <Image gridColumn="6 /13 " name="wales_02" margin="0%" />
+            <Image gridColumn="4 /13 " name="inflated_01" margin="20%" />
           </Layer>
           <Layer>
-            <Image gridColumn="2 /10 " name="inflated_01" margin="0%" />
+            <Video gridColumn="2 /6 " name="anim_02" margin="20%" />
           </Layer>
           <Layer>
-            <Video gridColumn="6 /13 " name="anim_02" margin="" />
+            <Image gridColumn="8 /12 " name="pdfe_02" margin="20%" />
+            <Image gridColumn="3 /6 " name="pdfe_01" margin="-60%" />
           </Layer>
           <Layer>
-            <Image gridColumn="2 /5 " name="pdfe_02" margin="0%" />
-            <Video gridColumn="7 /12 " name="pdfe_05" margin="40%" />
-          </Layer>
-          <Layer>
-            <Video gridColumn="2 /8 " name="pdfe_04" margin="0%" />
-          </Layer>
-          <Layer>
-            <Image gridColumn="1 /4 " name="pdfe_03" margin="100%" />
-            <Image gridColumn="6 /13 " name="pdfe_01" margin="0%" />
-          </Layer>
-          <Layer>
-            <Video gridColumn="2 /5 " name="anim_03" margin="0%" />
+            <Video gridColumn="1 /13 " name="pdfe_05" margin="10%" />
+            <Video gridColumn="1 /5 " name="pdfe_04" margin="10%" />
           </Layer>
           <Layer>
             <Video gridColumn="1 /7 " name="mtnslab_01" margin="30%" />
@@ -125,17 +115,7 @@ function App() {
             <Image gridColumn="3 /12 " name="chimie_01" margin="0%" />
           </Layer>
           <Layer>
-            <Video gridColumn="3 /13 " name="SBP_01" margin="0%" />
-          </Layer>
-          <Layer>
-            <Image gridColumn="1 /10 " name="etape_01" margin="0%" />
-          </Layer>
-          <Layer>
             <Video gridColumn="4 /12 " name="etape_01" margin="0%" />
-          </Layer>
-          <Layer>
-            <Image gridColumn="1 /6 " name="mutek_01" margin="0%" />
-            <Video gridColumn="8 /13 " name="mayman_03" margin="30%" />
           </Layer>
           <Layer>
             <Image gridColumn="1 /11 " name="mayman_02" margin="0%" />
